5cbe68478d8c2965456514bc50b3501e0206381b
Nguyễn Trọng Hiếu <tronghieu220403@gmail.com>
2023-10-19 23:47:24 +0700

    Update datadirectorytable.cpp

--- include/pestructure/datadirectories/datadirectorytable.cpp
+++ include/pestructure/datadirectories/datadirectorytable.cpp
@@ -10,104 +10,120 @@ namespace pe
 
     void DataDiretoryTable::SetDataDiretoryTable(const char *data_directories_data)
     {
-        int offset = -4;
+        int offset = 0;
         entry_vector_.clear();
 
         entry_vector_.push_back(DataDiretory(
             "Export Table",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])), 
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Import Table",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Resource Table",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Exception Table",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Certificate Table",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Base Relocation Table",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Debug",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Architecture",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Global Ptr",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "TLS Table",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Load Config Table",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Bound Import Table",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Import Address Table",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Delay-Load Import Tables",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "CLR Runtime Header",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
         entry_vector_.push_back(DataDiretory(
             "Reserved",
-            MemoryToUint32(&(data_directories_data[offset += 4])), 
-            MemoryToUint32(&(data_directories_data[offset += 4])))
+            MemoryToUint32(&(data_directories_data[offset])),
+            MemoryToUint32(&(data_directories_data[offset + 4])))
         );
+        offset += 8;
 
     }
     
